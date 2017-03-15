6dff65fe634028d12c4b42dda54336656d72f16a
meatyminx <flexayoung@gmail.com>
2017-03-15 11:38:18 +0000

    FlightStorage
    Nota þennan klassa sem tímabundinn gagnagrunn, getum núna farið að
    vinna með SearchManager og leitað að flugum

--- Flug/src/backEnd/FlightStorage.java
+++ Flug/src/backEnd/FlightStorage.java
@@ -1,14 +1,18 @@
 package backEnd;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 
 public class FlightStorage {
 
-	ArrayList <Flight> flightList = new ArrayList<Flight>();
+	static ArrayList <Flight> flightList = new ArrayList<Flight>();
 	@SuppressWarnings("deprecation")
 	static
 	ArrayList <Date> dateList = new ArrayList<Date>();
+	private static Random randomGenerator = new Random();
+	
 	
 	public static void main(String[] args){
 		for(int i = 0; i<100; i++){
@@ -22,15 +26,26 @@ public class FlightStorage {
 		
 		}
 		
-		for(int i = 0; i<100; i++)
-			System.out.println(dateList.get(i).toString());
-
-		//flightList.add( new Flight("FB0101", "Wow Air", (new Date(2017+1990, 5, 23, 0800, 45, 23))  ));
+		for(int i = 0; i<10; i++ ){
+			int index = randomGenerator.nextInt(dateList.size());
+			//baetir vid 2 klukkutima vid departureTime
+			Calendar cal =Calendar.getInstance();
+			cal.setTime(dateList.get(index));
+			cal.add(Calendar.HOUR_OF_DAY,2); // this will add two hours
+			Date myDate = cal.getTime();
+			
+			//bua til random verd
+			int verd = 8000 + (int)(Math.random()*100000);
+			
+			flightList.add( new Flight("FB0101", "Wow Air", dateList.get(index), myDate, "KEF", "JFK", true, verd, 30));		
+		}
 		
-		
-		
-		
+		for(int i = 0; i<10; i++ ){
+			
+		   System.out.println(flightList.get(i).getAirline() + " " + flightList.get(i).getPrice());
+		}
+		}
 		
 	}
 
-}
+
