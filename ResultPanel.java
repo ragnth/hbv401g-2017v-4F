8b63f4081decb26d779c8a68e7db1b8bbbb89098
meatyminx <flexayoung@gmail.com>
2017-04-06 20:11:35 +0000

    Add files via upload

---
+++ ResultPanel.java
@@ -0,0 +1,46 @@
+package frontEnd;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+import java.awt.Color;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class ResultPanel extends JPanel {
+	private JLabel logo;
+	/**
+	 * Create the panel.
+	 */
+	public ResultPanel() {
+		setSize(1000, 800);
+		setLayout(null);
+		setBackground(new Color(49,219,205));
+
+		logo = new JLabel("");
+		logo.setBounds(300, 20, 511, 330);
+		
+	   	add(logo);
+		logo.setIcon(new ImageIcon(getClass().getResource("/images/logoSmall.png")));
+		
+		/*
+		Icon icon = new ImageIcon(getClass().getResource("/images/giphy.gif"));
+		JLabel loadingGIF = new JLabel(icon);
+		loadingGIF.setBounds(300, 300, 300, 300);
+		add(loadingGIF);
+	
+		
+		*/
+		
+		//logo.setBorder(BorderFactory.createLineBorder(Color.white));
+
+		
+		
+		
+	}
+}
